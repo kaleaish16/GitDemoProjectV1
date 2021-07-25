8ec6b3bd09a2725287690644e8638417e1f954d4
Aishwarya Kale <kaleaish16@gmail.com>
2021-07-25 19:35:47 +0530

    This is my first commit...

---
+++ src/Test2.java
@@ -0,0 +1,18 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+		String s = "Aishwarya";
+		
+		if(s=="Aishwarya")
+		{
+			System.out.println("String is correct...");
+		}
+		else
+		{
+			System.out.println("Incorrect String...");
+		}
+	}
+
+}
---
+++ src/Test3.java
@@ -0,0 +1,21 @@
+
+public class Test3 {
+
+	public static void main(String[] args) {
+
+		String s = "Aishwarya";
+		
+		if(s=="Aishwarya")
+		{
+			System.out.println("String is correct...");
+		}
+		else
+		{
+			System.out.println("Incorrect String...");
+		}
+	}
+
+
+	}
+
+
